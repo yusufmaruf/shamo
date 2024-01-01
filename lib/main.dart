d5662e99ac491f82adb0ca8f15688ef534ec0d77
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-01 09:53:43 +0700

    import theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shamo/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,7 @@ class HomePage extends StatelessWidget {
         body: Center(
       child: Text(
         'Hello World',
-        style: GoogleFonts.poppins(fontSize: 50),
+        style: secondaryTextStyle.copyWith(fontSize: 30),
       ),
     ));
   }
