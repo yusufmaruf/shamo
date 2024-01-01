9ef99f0e67de03c1cca2f8860bca7fd8177c4e10
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-01 22:38:48 +0700

    header app bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello, Alex",
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    "@alexkeinn",
+                    style: subtitleTextStyle.copyWith(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/image_profile.png'),
+                  )),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
