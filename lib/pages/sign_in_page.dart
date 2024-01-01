796cde6ab1d63f843f3e08ea9a61556af6f8e203
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-01 14:32:22 +0700

    done sign in

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -76,8 +76,101 @@ class SignInPage extends StatelessWidget {
           ));
     }
 
+    Widget passwordInput() {
+      return Container(
+          margin: EdgeInsets.only(top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Password',
+                style:
+                    primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                height: 50,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'assets/icon_password.png',
+                        width: 17,
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Expanded(
+                          child: TextFormField(
+                        style: primaryTextStyle,
+                        obscureText: true,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Your Password',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ))
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ));
+    }
+
+    Widget signInButton() {
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 30),
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+              backgroundColor: primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              )),
+          child: Text(
+            'Sign In',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget footer() {
+      return Container(
+        margin: EdgeInsets.only(bottom: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Don\'t have an account? ',
+              style: subtitleTextStyle.copyWith(fontSize: 12),
+            ),
+            Text(
+              'Sign Up',
+              style: purpleTextStyle.copyWith(fontSize: 12, fontWeight: medium),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor1,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
           child: Container(
         margin: EdgeInsets.symmetric(horizontal: defaultMargin),
@@ -86,6 +179,10 @@ class SignInPage extends StatelessWidget {
           children: [
             header(),
             emailInput(),
+            passwordInput(),
+            signInButton(),
+            Spacer(),
+            footer(),
           ],
         ),
       )),
