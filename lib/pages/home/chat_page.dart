6d5ec3bcb93443d908fcdecf995b318ede9b9e58
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-03 13:19:24 +0700

    empty

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Chat Page'),
-    );
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Message Suport',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/icon_headset.png', width: 80),
+              SizedBox(height: 20),
+              Text(
+                'Opss no message yet?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 15,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(height: 12),
+              Text('Anda dapat menghubungi kami melalui WhatsApp',
+                  style: secondaryTextStyle),
+              SizedBox(height: 20),
+              Container(
+                  height: 44,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 10,
+                      ),
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text(
+                      'Hubungi Kami',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(children: [
+      header(),
+      content(),
+    ]);
   }
 }
