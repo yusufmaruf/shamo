fff42d59d5c836e95095d5111e90e78eaade9d67
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-05 23:48:13 +0700

    finish slicing chat page

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -149,6 +149,7 @@ class DetailChatPage extends StatelessWidget {
           ChatBubble(
             isSender: true,
             text: 'Hi, This item is still available',
+            hasProduct: true,
           ),
           ChatBubble(
             isSender: false,
