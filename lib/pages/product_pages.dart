c51580fa529d33de37a2c7e9049b24a98c99b256
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-06 14:26:55 +0700

    carousel image setup

--- lib/pages/product_pages.dart
+++ lib/pages/product_pages.dart
@@ -1,12 +1,32 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
-class ProductPage extends StatelessWidget {
-  const ProductPage({super.key});
+class ProductPage extends StatefulWidget {
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List images = ['assets/image_shoes.png', 'assets/image_shoes.png'];
+  int cureentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Widget indicator(int index) {
+      return Container(
+        width: cureentIndex == index ? 16 : 4,
+        height: 4,
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: cureentIndex == index ? primaryColor : Color(0xffC4C5C5),
+        ),
+      );
+    }
+
     Widget header() {
+      int index = -1;
       return Column(
         children: [
           Container(
@@ -28,6 +48,35 @@ class ProductPage extends StatelessWidget {
               ],
             ),
           ),
+          CarouselSlider(
+            items: images
+                .map(
+                  (image) => Image.asset(
+                    image,
+                    width: MediaQuery.of(context).size.width,
+                    height: 310,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+                initialPage: 0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    cureentIndex = index;
+                  });
+                }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map((e) {
+              index++;
+              return indicator(index);
+            }).toList(),
+          )
         ],
       );
     }
