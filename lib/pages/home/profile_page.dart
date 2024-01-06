c0b87f877fef2968380f1eadad98c95a5a582784
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-06 10:33:04 +0700

    profile page headr

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
-    );
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(
+              defaultMargin,
+            ),
+            child: Row(
+              children: [
+                ClipOval(
+                    child: Image.asset(
+                  'assets/image_profile.png',
+                  width: 64,
+                )),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Alex',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        '@alexkeinn',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/button_exit.png',
+                  width: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(children: [
+      header(),
+    ]);
   }
 }
